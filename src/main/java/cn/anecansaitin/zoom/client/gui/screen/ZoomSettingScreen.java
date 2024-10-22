4822f6293a23a5474174f855f9ebf5a486b7ee63
AnECanSaiTin <cyyde@163.com>
2024-10-22 18:10:30 +0800

    ①为自由模式与第一人称+模式添加滚筒角调整 ②为第一人称添加fov调整

--- src/main/java/cn/anecansaitin/zoom/client/gui/screen/ZoomSettingScreen.java
+++ src/main/java/cn/anecansaitin/zoom/client/gui/screen/ZoomSettingScreen.java
@@ -3,6 +3,7 @@ package cn.anecansaitin.zoom.client.gui.screen;
 import cn.anecansaitin.zoom.client.ZoomClientConfig;
 import cn.anecansaitin.zoom.client.ZoomKeyMapping;
 import cn.anecansaitin.zoom.client.gui.overlay.ZoomOverlay;
+import cn.anecansaitin.zoom.client.listener.FirstPersonPlus;
 import cn.anecansaitin.zoom.client.listener.FreeMode;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Font;
@@ -107,7 +108,9 @@ public class ZoomSettingScreen extends Screen {
             guiGraphics.drawString(font, Component.translatable("gui.free_mode_overlay.mode_on", ZoomKeyMapping.FREE_MODE.get().getKey().getDisplayName()), x, y, 0xFFFFFF);
             guiGraphics.drawString(font, Component.translatable("gui.free_mode_overlay.move_mode", FreeMode.MOVE_MODE ? Component.translatable("gui.free_mode_overlay.move_mode.cross_hair") : Component.translatable("gui.free_mode_overlay.move_mode.normal"), ZoomKeyMapping.MOVE_MODE.get().getKey().getDisplayName()), x, y + 10, 0xFFFFFF);
             guiGraphics.drawString(font, Component.translatable("gui.free_mode_overlay.speed", String.format("%.2f", FreeMode.SPEED).substring(2)), x, y + 20, 0xFFFFFF);
-            guiGraphics.drawString(Minecraft.getInstance().font, Component.translatable("gui.free_mode_overlay.fov", String.format("%.0f", FreeMode.FOV), ZoomKeyMapping.FOV_UP.get().getKey().getDisplayName(), ZoomKeyMapping.FOV_DOWN.get().getKey().getDisplayName()), x, y + 30, 0xFFFFFF);
+            guiGraphics.drawString(font, Component.translatable("gui.common_overlay.fov", String.format("%.0f", FreeMode.getFov()), ZoomKeyMapping.FOV_UP.get().getKey().getDisplayName(), ZoomKeyMapping.FOV_DOWN.get().getKey().getDisplayName()), x, y + 30, 0xFFFFFF);
+            guiGraphics.drawString(font, Component.translatable("gui.common_overlay.z_rot", String.format("%.0f", FreeMode.getZRot()), ZoomKeyMapping.Z_ROT_CLOCKWISE.get().getKey().getDisplayName(), ZoomKeyMapping.Z_ROT_ANTICLOCKWISE.get().getKey().getDisplayName()), x, y + 40, 0xFFFFFF);
+
         }
 
         @Override
@@ -150,6 +153,8 @@ public class ZoomSettingScreen extends Screen {
             Font font = Minecraft.getInstance().font;
             guiGraphics.drawString(font, Component.translatable("gui.fps_mode_overlay.mode_on", ZoomKeyMapping.FPS_PLUS_MODE.get().getKey().getDisplayName()), x, y, 0xFFFFFF);
             guiGraphics.drawString(font, Component.translatable("gui.fps_mode_overlay.turn_head", ZoomKeyMapping.TURN_HEAD.get().getKey().getDisplayName()), x, y + 10, 0xFFFFFF);
+            guiGraphics.drawString(Minecraft.getInstance().font, Component.translatable("gui.common_overlay.fov", String.format("%.0f", FirstPersonPlus.getFov()), ZoomKeyMapping.FOV_UP.get().getKey().getDisplayName(), ZoomKeyMapping.FOV_DOWN.get().getKey().getDisplayName()), x, y + 20, 0xFFFFFF);
+            guiGraphics.drawString(font, Component.translatable("gui.common_overlay.z_rot", String.format("%.0f", FirstPersonPlus.getZRot()), ZoomKeyMapping.Z_ROT_CLOCKWISE.get().getKey().getDisplayName(), ZoomKeyMapping.Z_ROT_ANTICLOCKWISE.get().getKey().getDisplayName()), x, y + 30, 0xFFFFFF);
         }
 
         @Override
